dd1338fcf8128e05e05c0973c8463ca0b0fcc1cc
Bharath Kumar <68462057+BharathBK05@users.noreply.github.com>
2023-05-03 18:20:22 +0530

    hello

--- index.js
+++ index.js
@@ -124,10 +124,8 @@ app.get("/posts/:postId", function(req, res){
 
 
 let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
 
-app.listen(port, function() {
+
+app.listen(port || 9999, function() {
   console.log("Server has started successfully");
 });

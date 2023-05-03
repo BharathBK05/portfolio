9ccb20062610af9a043ee434a0ce8a42836fa72d
Bharath Kumar <68462057+BharathBK05@users.noreply.github.com>
2023-05-03 18:26:34 +0530

    hello

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const ejs = require("ejs");
 const _ = require("lodash");
 
 const app = express();
-
+app.set('views', './views')
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
